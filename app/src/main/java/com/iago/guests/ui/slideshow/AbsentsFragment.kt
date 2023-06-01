646a652cb0484011df2886ae2b38a51783708818
Iago Gondim <iago-gondim@hotmail.com>
2023-06-01 07:46:19 -0300

    first commit

---
+++ app/src/main/java/com/iago/guests/ui/slideshow/AbsentsFragment.kt
@@ -0,0 +1,39 @@
+package com.iago.guests.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.iago.guests.databinding.FragmentAbsentsBinding
+
+class AbsentsFragment : Fragment() {
+  
+  private var _binding: FragmentAbsentsBinding? = null
+  private val binding get() = _binding!!
+  
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    val viewModel =
+      ViewModelProvider(this).get(AbsentsViewModel::class.java)
+    
+    _binding = FragmentAbsentsBinding.inflate(inflater, container, false)
+    val root: View = binding.root
+    
+    val textView: TextView = binding.textAbsents
+    viewModel.text.observe(viewLifecycleOwner) {
+      textView.text = it
+    }
+    return root
+  }
+  
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
+}
